af68bf9c409057e5474142c55cb1cc6afb7871d1
John Palomo <jmpalomo@uci.edu>
2015-01-07 10:34:04 -0800

    Initial commit of project structure with simple-single test case.

---
+++ pl241_compiler/src/test/java/pl241_compiler/TestParser.java
@@ -0,0 +1,12 @@
+package pl241_compiler;
+
+import org.junit.Test;
+
+public class TestParser
+{
+    @Test
+    public void testTest()
+    {
+        System.out.println("This is a test");
+    }
+}
